de8ff4c480a50c912ed1a5dbee9eb3e686fcb952
Jan Lerking <jan.lerking@gmail.com>
2022-04-07 16:21:46 +0200

    Updated. /JL

--- examples/json.go
+++ examples/json.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 )
 
+type Salary struct {
+	Basic, HRA, TA float64
+}
+
+type Employee struct {
+	FirstName, LastName, Email string
+	Age                        int
+	MonthlySalary              []Salary
+}
+
 func main() {
-	fileCount := map[string]int{
-		"cpp": 10,
-		"js":  8,
-		"go":  10,
+	data := Employee{
+		FirstName: "Mark",
+		LastName:  "Jones",
+		Email:     "mark@gmail.com",
+		Age:       25,
+		MonthlySalary: []Salary{
+			Salary{
+				Basic: 15000.00,
+				HRA:   5000.00,
+				TA:    2000.00,
+			},
+			Salary{
+				Basic: 16000.00,
+				HRA:   5000.00,
+				TA:    2100.00,
+			},
+			Salary{
+				Basic: 17000.00,
+				HRA:   5000.00,
+				TA:    2200.00,
+			},
+		},
 	}
-	bytes, _ := json.Marshal(fileCount)
-	fmt.Println(string(bytes))
+
+	file, _ := json.MarshalIndent(data, "", " ")
+
+	_ = ioutil.WriteFile("test.json", file, 0644)
 }
